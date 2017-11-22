ec662d127036ba4c884919af84eae1bfb4ff1874
MSI\noah <23075@ma-web.nl>
2017-11-22 15:48:56 +0100

    alles

---
+++ script/restart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour {
+
+    private DrawScript draw;
+
+    public void RestartGame()
+    {
+        
+        Application.LoadLevel(0);
+    }
+}
